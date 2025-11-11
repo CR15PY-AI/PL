f7ea46de09769d074398ba0c4037f22b5cf49a75
CR15PY-AI <a.azamat2007@mail.ru>
2025-11-12 01:09:37 +0600

    Add files via upload

---
+++ HomeWork8.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class HomeWork8 {
+//    #######################SUM NUMBERS##############################
+//    public static void main(String[] args) {
+//        int [] numbers = {1,2,3,4,5,6,7,8,9,10};
+//        int sum = 0;
+//        for (int i : numbers) {
+//            if (i % 2 != 0) {
+//                System.out.println(i);
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);
+//    }
+//       #####################SEARCH ELEMENT##########################
+//        public static void main(String[] args) {
+//            Scanner sc = new Scanner(System.in);
+//            int [] numbers = {1,2,3,4,5,6,7,8,9,10};
+//            System.out.print("Enter a number : ");
+//            int target = sc.nextInt();
+//            for (int i = 0; i < numbers.length; i++){
+//                if(numbers[i] == target){
+//                    System.out.println("index: " + i);
+//                    break;
+//                } else {
+//                    System.out.println("not found");
+//                    break;
+//                }
+//            }
+//        }
+// #########################COPY ARRAY#########################
+//        public static void main(String[] args) {
+//            int [] orig = {1,2,3,4,5,6,7,8,9,10,11};
+//            int [] copy = new int[orig.length];
+//
+//            for (int i = 0; i < orig.length; i++){
+//                copy[i] = orig[i];
+//            }
+//            System.out.println("Original : " + Arrays.toString(orig));
+//            System.out.println("Copy : " + Arrays.toString(copy));
+//        }
+//#####################ARRAY INPUT#############################
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter number of elements: ");
+//        int size = sc.nextInt();
+//
+//        int[] arr = new int[size];
+//
+//        for (int i = 0; i < size; i++) {
+//            System.out.print("Enter value for element " + (i + 1) + ": ");
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        System.out.println("You entered:");
+//        for (int num : arr) {
+//            System.out.print(num + " ");
+//        }
+//    }
+//###################FAVORITE THINGS########################
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    String[] favorites = new String[3];
+//
+//    for (int i = 0; i < favorites.length; i++) {
+//        System.out.print("Enter your favorite item " + (i + 1) + ": ");
+//        favorites[i] = sc.nextLine();
+//    }
+//
+//    System.out.println("\nYour favorite items are:");
+//    for (String item : favorites) {
+//        System.out.println(item);
+//    }
+}
+
+
+
